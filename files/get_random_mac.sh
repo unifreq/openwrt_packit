c7649315dfad1cc8c0ec5c464134612829b54da4
unifreq <uniqfreq@gmail.com>
2021-04-17 17:02:23 +0800

    限定随机MAC地址第一段

--- files/get_random_mac.sh
+++ files/get_random_mac.sh
@@ -2,11 +2,14 @@
   
 get_random_mac ()
 {
+    # MAC地址第一段可在 02 06 0A 0E 中任选一个
     if [ "$SHELL" == "/bin/bash" ];then
-        MACADDR=$(printf "%02X:%02X:%02X:%02X:%02X:%02X\n" $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255])
+        #MACADDR=$(printf "%02X:%02X:%02X:%02X:%02X:%02X\n" $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255])
+        MACADDR=$(printf "06:%02X:%02X:%02X:%02X:%02X\n" $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255] $[RANDOM%255])
     else
         uuid=$(cat /proc/sys/kernel/random/uuid)
-        mac1=${uuid:24:2}
+        mac1="0E"
+        #mac1=${uuid:24:2}
         mac2=${uuid:26:2}
         mac3=${uuid:28:2}
         mac4=${uuid:30:2}
