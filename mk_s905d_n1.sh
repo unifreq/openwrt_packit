21c5a0a921b41d0248e183394a589d295481c404
unifreq <uniqfreq@gmail.com>
2021-05-01 12:58:32 +0800

    change anon mount option

--- mk_s905d_n1.sh
+++ mk_s905d_n1.sh
@@ -460,7 +460,7 @@ cat > ./etc/config/fstab <<EOF
 config global
         option anon_swap '0'
         option auto_swap '0'
-        option anon_mount '0'
+        option anon_mount '1'
         option auto_mount '1'
         option delay_root '5'
         option check_fs '0'
