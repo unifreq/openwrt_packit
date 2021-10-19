158e21ab37a13c04bb6c1986bd64461be9a719a0
unifreq <flippy@sina.com>
2021-10-19 21:37:36 +0800

    Add log details for first_run.sh

--- files/first_run.sh
+++ files/first_run.sh
@@ -70,8 +70,12 @@ t
 w
 EOF
 
+echo "Backup the partition table ... "
 dd if=/dev/$DISK_NAME of=/tmp/partition.bak bs=512 count=1
 sync
+echo "done"
+
+echo "fdisk starting ... "
 fdisk /dev/$DISK_NAME < /tmp/fdisk.script
 if [ $? -ne 0 ];then
         echo "fdisk failed, restore the backup bootloader, and abort"
@@ -113,9 +117,11 @@ case $TARGET_SHARED_FSTYPE in
              mount -t ext4    "/dev/${PT_PRE}4" "/mnt/${PT_PRE}4"
              ;;
 esac
+echo "done"
 
 # 新分区建立成功后, 允许在非EMMC设备上也启用docker
 # init dockerd
+echo "Init the dockerd configs ... "
 mkdir -p "/mnt/${PT_PRE}4/docker"
 rm -rf "/opt/docker"
 ln -sf "/mnt/${PT_PRE}4/docker/" "/opt/docker"
@@ -136,12 +142,25 @@ cat > /etc/docker/daemon.json <<EOF
    ]
 }
 EOF
+echo "done"
+echo -n "stop dockerd ... "
 /etc/init.d/dockerd stop
+echo "ok"
+
+echo -n "disable dockerd ... "
 /etc/init.d/dockerd disable
+echo "ok"
+
+echo -n "starting dockerd ... "
 /etc/init.d/dockerd start
+echo "ok"
+
+echo -n "enable dockerd ... "
 /etc/init.d/dockerd enable
+echo "ok"
 
 # init AdguardHome
+echo "Init the Adguard config ... "
 if [ -f /etc/config/AdGuardHome ];then
     mkdir -p "/mnt/${PT_PRE}4/AdGuardHome/data"
     rm -rf "/usr/bin/AdGuardHome"
@@ -149,4 +168,7 @@ if [ -f /etc/config/AdGuardHome ];then
 fi
 sync
 echo "done"
+echo "clean ... "
 destory_myself
+echo "done"
+echo "The end."
