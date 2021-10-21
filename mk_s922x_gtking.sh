abbb4197e0ebf9940cb753a382b736687c3770d4
unifreq <flippy@sina.com>
2021-10-21 14:42:25 +0800

    修改s922x的说明文字

--- mk_s922x_gtking.sh
+++ mk_s922x_gtking.sh
@@ -139,16 +139,20 @@ INITRD=/uInitrd
 
 # 下列 dtb，用到哪个就把哪个的#删除，其它的则加上 # 在行首
 
-# 用于 Belink GT-King (1896Mhz/1800Mhz)
+# S922X 标准版
+# 用于 Belink GT-King (1800Mhz/1908Mhz)
 #FDT=/dtb/amlogic/meson-g12b-gtking.dtb
 
-# 用于 Belink GT-King (2016Mhz/2016Mhz)
+# S922X 超频版 
+# 用于 Belink GT-King (1992Mhz/1992Mhz)
 #FDT=/dtb/amlogic/meson-g12b-gtking-oc.dtb
 
-# 用于 Belink GT-King Pro ( 1800Mhz/2208Mhz)
+# S922X-H 标准版 
+# 用于 Belink GT-King Pro (1800Mhz/2208Mhz)
 FDT=/dtb/amlogic/meson-g12b-gtking-pro.dtb
 
-# 用于 Belink GT-King Pro ( 2016Mhz/2208Mhz)
+# S922X-H 超频版
+# 用于 Belink GT-King Pro (2016Mhz/2208Mhz)
 FDT=/dtb/amlogic/meson-g12b-gtking-pro-oc.dtb
 
 APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
--- mk_s922x_odroid-n2.sh
+++ mk_s922x_odroid-n2.sh
@@ -139,11 +139,17 @@ INITRD=/uInitrd
 
 # 下列 dtb，用到哪个就把哪个的#删除，其它的则加上 # 在行首
 
-# 用于 Hardkernel Odroid N2 (1896Mhz/1800Mhz)
+# 标准版
+# 用于 Hardkernel Odroid N2 (1800Mhz/1908Mhz)
 #FDT=/dtb/amlogic/meson-g12b-odroid-n2.dtb
 
-# 用于 Belink GT-King Pro (Rev A) ( 1896Mhz/1800Mhz)
+# 标准版
+# 用于 Belink GT-King Pro (Rev A) (1800Mhz/1908Mhz)
 FDT=/dtb/amlogic/meson-g12b-gtking-pro-rev_a.dtb
+
+# 超频版
+# 用于 Belink GT-King Pro (Rev A) (1992Mhz/1992Mhz)
+#FDT=/dtb/amlogic/meson-g12b-gtking-pro-rev_a-oc.dtb
 
 APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
 EOF
