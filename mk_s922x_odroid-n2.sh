28f93aacec3ffd532f29dd50f1f278a967f8a774
unifreq <flippy@sina.com>
2021-12-23 00:14:48 +0800

    Adjust mosdns config

--- mk_s922x_odroid-n2.sh
+++ mk_s922x_odroid-n2.sh
@@ -183,6 +183,7 @@ use_xrayplug_replace_v2rayplug
 create_fstab_config
 adjust_turboacc_config
 adjust_ntfs_config
+adjust_mosdns_config
 patch_admin_status_index_html
 adjust_kernel_env
 copy_uboot_to_fs
