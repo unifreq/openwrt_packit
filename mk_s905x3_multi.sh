68d444beddea01e3fbd91495ce4ff2587859e411
unifreq <flippy@sina.com>
2022-03-15 23:54:29 +0800

    Board: S905X3: Add Tanix TX3 support

--- mk_s905x3_multi.sh
+++ mk_s905x3_multi.sh
@@ -189,6 +189,18 @@ FDT=/dtb/amlogic/meson-sm1-x96-max-plus-100m.dtb
 # 用于 A95XF3 air 百兆版
 #FDT=/dtb/amlogic/meson-sm1-a95xf3-air-100.dtb
 
+# 用于 Tanix TX3 百兆版
+#FDT=/dtb/amlogic/meson-sm1-tx3-bz.dtb
+
+# 用于 Tanix TX3 百兆版 (超频至2208Mhz)
+#FDT=/dtb/amlogic/meson-sm1-tx3-bz-oc.dtb
+
+# 用于 Tanix TX3 千兆版
+#FDT=/dtb/amlogic/meson-sm1-tx3-qz.dtb
+
+# 用于 Tanix TX3 千兆版 (超频至2208Mhz)
+#FDT=/dtb/amlogic/meson-sm1-tx3-qz-oc.dtb
+
 APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd:${ZSTD_LEVEL} console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
 EOF
 
