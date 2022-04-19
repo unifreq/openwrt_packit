e9f2ea3479e4ce68cbfeb81b4ad8403e5d744346
unifreq <flippy@sina.com>
2022-04-20 00:06:28 +0800

    ss-benchmark-local.sh: some improvements

--- files/ss-benchmark-local.sh
+++ files/ss-benchmark-local.sh
@@ -3,6 +3,10 @@
 # 测试文件默认内存的 1/4，放在/tmp(内存)下面，请事先确认空间是否足够
 free_mem=$(df -m /tmp | tail -1 | awk '{print $2}')
 bin_size=$((free_mem / 4))
+BIN_HOME=/usr/bin
+# 如果在linux中测试，需开启 nginx 服务，并设置 WWW_HOME 为实际的 www_root 目录
+# example: armbian: /var/www/html
+WWW_HOME=/www
 
 function create_test_json() {
     local jsonfile=$1
@@ -19,9 +23,6 @@ function create_test_json() {
 }
 EOF
 }
-
-BIN_HOME=/usr/bin
-WWW_HOME=/www
 
 SS_SERVER=${BIN_HOME}/ss-server
 SS_LOCAL=${BIN_HOME}/ss-local
@@ -104,12 +105,27 @@ mount -o bind /tmp/test ${WWW_HOME}/test
 echo "done"
 echo 
 
+TIMEFORMAT='%3R'
 retfile=$(mktemp)
+
+function on_trap_exit() {
+    killall curl 2>/dev/null
+    while umount ${WWW_HOME}/test 2>/dev/null;do
+        echo -n "umount ${WWW_HOME}/test ... "
+    done
+    echo "done"
+    echo -n "cleaning test files ... "
+    rm -rf /tmp/test ${WWW_HOME}/test /tmp/ss_test.json $retfile
+    echo "done"
+    exit 0
+}
+trap on_trap_exit 2 3 15
+
 echo " benchmark begin ... "
 echo "==============================================================================="
 for method in $methods;do
     echo 
-    echo "-------------->>>>>>>>>>>>>  method: $method"
+    echo -e "-------------->>>>>>>>>>>>>  method: \033[33m$method\033[0m"
     echo "start ss-server ... "
     create_test_json "/tmp/ss_test.json" "${method}"
     $SS_SERVER -c /tmp/ss_test.json &
@@ -123,14 +139,18 @@ for method in $methods;do
     echo
     echo -n "start curl download benchmark ... "
     curltmp=$(mktemp)
-    curl --socks5 127.0.0.1 http://localhost/test/test.bin --output /dev/null 2>$curltmp
+    timetmp=$(mktemp)
+    { time curl --socks5 127.0.0.1 http://127.0.0.1/test/test.bin --output /dev/null 2>$curltmp;} 2>$timetmp
     if [ $? -eq 0 ];then
 	echo "ok"
+	realtime=$(cat $timetmp)
+	echo "Downloading ${bin_size}MB of data took ${realtime} seconds"
 	echo "--------------------------------------------------------------------------------"
 	cat $curltmp | tr '\r' '\n'
 	echo "--------------------------------------------------------------------------------"
-	perf=$(cat $curltmp | tr '\r' '\n' | tail -n1 | tr -d 'M' | awk '{printf("%0.1fMB/s\n",$7)}')
-        printf "%-25s->%12s\n" "$method" "$perf" >> $retfile
+	perf1=$(cat $curltmp | tr '\r' '\n' | tail -n1 | tr -d 'M' | awk '{printf("%0.1f\n",$7)}')
+	perf2=$(echo "$bin_size $realtime" | awk '{printf("%0.2f\n", $1/$2)}')
+        printf "%-25s->%12s%12s\n" "$method" "$perf1" "$perf2" >> $retfile
     else
 	echo "failed!"
 
@@ -148,17 +168,19 @@ while umount ${WWW_HOME}/test 2>/dev/null;do
     echo -n "umount ${WWW_HOME}/test ... "
 done
 echo "done"
-echo -n "cleaning test file ... "
+echo -n "cleaning test files ... "
 rm -rf /tmp/test ${WWW_HOME}/test
 echo "done"
 echo
 echo
-echo " <<<  The benchmark result report  >>>" 
-echo "+++++++++++++++++++++++++++++++++++++++"
+echo -e "      \033[32m<<<  The benchmark result report  >>>\033[0m" 
+echo "---------------------------------------------------"
+printf "\033[33m%-25s  %12s%12s\033[0m\n" "Method name" "Curl rpt" "size/time"
+printf "\033[31m%-25s  %12s%12s\033[0m\n" " " "(MB/s)" "(MB/s)"
+echo "---------------------------------------------------"
 cat $retfile
-rm -f $retfile
-echo "+++++++++++++++++++++++++++++++++++++++"
-echo "ss version: [$SS_VERSION]"
+echo "---------------------------------------------------"
+echo -e "Shadowsocks version: [\033[32m$SS_VERSION\033[0m]"
 model="unknown"
 arch=$(uname -m)
 if [ "$arch" == "aarch64" ];then
@@ -166,8 +188,10 @@ if [ "$arch" == "aarch64" ];then
 elif [ "$arch" == "x86_64" ];then
     model=$(cat /proc/cpuinfo | grep 'model name' | uniq | awk -F':' '{print $2}') 
 fi
-echo "model: [$model]"
+echo -e "Model: [\033[32m$model\033[0m]"
 kernel=$(uname -r)
-echo "kernel: [$kernel]"
-echo "注意： 以上结果仅用于评估单核性能，不代表实际网速"
+echo -e "Kernel: [\033[32m$kernel\033[0m]"
+echo -e "\033[33m注：以上结果仅用于评估单核性能，不代表实际网速\033[0m"
 echo
+rm -f $retfile /tmp/ss_test.json
+exit
