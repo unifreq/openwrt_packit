60c7ea47146d2f6c3f2692b849a0f3e8bee6e87c
ffuqiangg <ffuqiangg@gmail.com>
2022-10-25 15:20:29 +0800

    update 2022.10.25

---
+++ mk_openwrt_n1.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+echo "========================= begin $0 ==========================="
+source make.env
+source public_funcs
+source my_pubfuncs
+init_work_env
+
+# 盒子型号识别参数 
+PLATFORM=amlogic
+SOC=s905d
+BOARD=n1
+
+# 让N1一直有wifi可用，以减少抱怨
+# 5.10(及以上)内核是否启用wifi  1:启用 0:禁用
+ENABLE_WIFI_K510=0
+
+SUBVER=$1
+
+# Kernel image sources
+###################################################################
+MODULES_TGZ=${KERNEL_PKG_HOME}/modules-${KERNEL_VERSION}.tar.gz
+check_file ${MODULES_TGZ}
+BOOT_TGZ=${KERNEL_PKG_HOME}/boot-${KERNEL_VERSION}.tar.gz
+check_file ${BOOT_TGZ}
+DTBS_TGZ=${KERNEL_PKG_HOME}/dtb-amlogic-${KERNEL_VERSION}.tar.gz
+check_file ${DTBS_TGZ}
+K510=$(get_k510_from_boot_tgz "${BOOT_TGZ}" "vmlinuz-${KERNEL_VERSION}")
+export K510
+###########################################################################
+
+# Openwrt root 源文件
+OP_ROOT_TGZ="openwrt-armvirt-64-default-rootfs.tar.gz"
+OPWRT_ROOTFS_GZ="${PWD}/${OP_ROOT_TGZ}"
+check_file ${OPWRT_ROOTFS_GZ}
+echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
+
+# 目标镜像文件
+TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${SUBVER}.img"
+
+# 补丁和脚本
+###########################################################################
+REGULATORY_DB="${PWD}/files/regulatory.db.tar.gz"
+KMOD="${PWD}/files/kmod"
+KMOD_BLACKLIST="${PWD}/files/kmod_blacklist"
+MAC_SCRIPT1="${PWD}/files/fix_wifi_macaddr.sh"
+MAC_SCRIPT2="${PWD}/files/find_macaddr.pl"
+MAC_SCRIPT3="${PWD}/files/inc_macaddr.pl"
+CPUSTAT_SCRIPT="${PWD}/files/cpustat"
+CPUSTAT_SCRIPT_PY="${PWD}/files/cpustat.py"
+INDEX_PATCH_HOME="${PWD}/myfiles/index.html.patches"
+GETCPU_SCRIPT="${PWD}/files/getcpu"
+TTYD="${PWD}/files/ttyd"
+FLIPPY="${PWD}/files/scripts_deprecated/flippy_cn"
+BANNER="${PWD}/myfiles/banner"
+
+# 20200314 add
+FMW_HOME="${PWD}/files/firmware"
+SMB4_PATCH="${PWD}/files/smb4.11_enable_smb1.patch"
+SYSCTL_CUSTOM_CONF="${PWD}/files/99-custom.conf"
+
+# 20200709 add
+COREMARK="${PWD}/files/coremark.sh"
+
+# 20200930 add
+SND_MOD="${PWD}/files/s905d/snd-meson-gx"
+DAEMON_JSON="${PWD}/files/s905d/daemon.json"
+
+# 20201006 add
+FORCE_REBOOT="${PWD}/files/s905d/reboot"
+# 20201017 add
+BAL_ETH_IRQ="${PWD}/files/balethirq.pl"
+# 20201026 add
+FIX_CPU_FREQ="${PWD}/files/fixcpufreq.pl"
+SYSFIXTIME_PATCH="${PWD}/files/sysfixtime.patch"
+
+# 20201128 add
+SSL_CNF_PATCH="${PWD}/files/openssl_engine.patch"
+
+# 20201212 add
+BAL_CONFIG="${PWD}/files/s905d/balance_irq"
+CPUFREQ_INIT="${PWD}/files/s905d/cpufreq"
+
+# 20210302 modify
+FIP_HOME="${PWD}/files/meson_btld/with_fip/s905d"
+UBOOT_WITH_FIP="${FIP_HOME}/n1-u-boot.bin.sd.bin"
+UBOOT_WITHOUT_FIP_HOME="${PWD}/files/meson_btld/without_fip"
+UBOOT_WITHOUT_FIP="u-boot-n1.bin"
+
+# 20210208 add
+WIRELESS_CONFIG="${PWD}/files/s905d/wireless"
+
+# 20210307 add
+SS_LIB="${PWD}/files/ss-glibc/lib-glibc.tar.xz"
+SS_BIN="${PWD}/files/ss-glibc/armv8a_crypto/ss-bin-glibc.tar.xz"
+JQ="${PWD}/files/jq"
+
+# 20210330 add
+DOCKERD_PATCH="${PWD}/files/dockerd.patch"
+
+# 20200416 add
+FIRMWARE_TXZ="${PWD}/files/firmware_armbian.tar.xz"
+BOOTFILES_HOME="${PWD}/files/bootfiles/amlogic"
+GET_RANDOM_MAC="${PWD}/files/get_random_mac.sh"
+
+# 20210618 add
+DOCKER_README="${PWD}/files/DockerReadme.pdf"
+
+# 20210704 add
+SYSINFO_SCRIPT="${PWD}/myfiles/30-sysinfo.sh"
+
+# 20210923 add
+OPENWRT_INSTALL="${PWD}/files/openwrt-install-amlogic"
+OPENWRT_UPDATE="${PWD}/files/openwrt-update-amlogic"
+OPENWRT_KERNEL="${PWD}/files/openwrt-kernel"
+OPENWRT_BACKUP="${PWD}/files/openwrt-backup"
+
+# 20211019 add
+FIRSTRUN_SCRIPT="${PWD}/files/first_run.sh"
+
+# 20211020 add
+BTLD_BIN="${PWD}/files/s905d/u-boot-2015-phicomm-n1.bin"
+
+# 20211024 add
+MODEL_DB="${PWD}/files/amlogic_model_database.txt"
+# 20211214 add
+P7ZIP="${PWD}/files/7z"
+# 20211217 add
+DDBR="${PWD}/files/openwrt-ddbr"
+# 20220225 add
+SSH_CIPHERS="aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr,chacha20-poly1305@openssh.com"
+SSHD_CIPHERS="aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr"
+###########################################################################
+
+check_depends
+
+SKIP_MB=4
+BOOT_MB=160
+ROOTFS_MB=736
+SIZE=$((SKIP_MB + BOOT_MB + ROOTFS_MB))
+create_image "$TGT_IMG" "$SIZE"
+create_partition "$TGT_DEV" "msdos" "$SKIP_MB" "$BOOT_MB" "fat32" "0" "-1" "btrfs"
+make_filesystem "$TGT_DEV" "B" "fat32" "BOOT" "R" "btrfs" "ROOTFS"
+mount_fs "${TGT_DEV}p1" "${TGT_BOOT}" "vfat"
+mount_fs "${TGT_DEV}p2" "${TGT_ROOT}" "btrfs" "compress=zstd:${ZSTD_LEVEL}"
+echo "创建 /etc 子卷 ..."
+btrfs subvolume create $TGT_ROOT/etc
+extract_rootfs_files
+extract_amlogic_boot_files
+
+echo "修改引导分区相关配置 ... "
+cd $TGT_BOOT
+rm -f uEnv.ini
+cat > uEnv.txt <<EOF
+LINUX=/zImage
+INITRD=/uInitrd
+
+# 下列 dtb，用到哪个就把哪个的#删除，其它的则加上 # 在行首
+
+# 用于 Phicomm N1
+FDT=/dtb/amlogic/meson-gxl-s905d-phicomm-n1.dtb
+
+# 用于 Phicomm N1 (thresh)
+#FDT=/dtb/amlogic/meson-gxl-s905d-phicomm-n1-thresh.dtb
+
+APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd:${ZSTD_LEVEL} console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
+EOF
+
+echo "uEnv.txt -->"
+echo "==============================================================================="
+cat uEnv.txt
+echo "==============================================================================="
+echo
+
+echo "修改根文件系统相关配置 ... "
+cd $TGT_ROOT
+copy_supplement_files
+sed -i '/# Update release file/a\    flippy_ver=$(uname -r)' ./usr/sbin/openwrt-kernel
+sed -i 's| Kernel.*/ Kernel: |${flippy_ver}/|' ./usr/sbin/openwrt-kernel
+extract_glibc_programs
+adjust_docker_config
+adjust_openssl_config
+adjust_qbittorrent_config
+adjust_getty_config
+my_adjust_samba_config
+adjust_nfs_config "mmcblk2p4"
+adjust_openssh_config
+adjust_openclash_config
+use_xrayplug_replace_v2rayplug
+create_fstab_config
+adjust_turboacc_config
+adjust_ntfs_config
+adjust_mosdns_config
+patch_admin_status_index_html
+adjust_kernel_env
+copy_uboot_to_fs
+patch_vimrc
+patch_luci_network
+write_release_info
+my_write_banner 
+config_first_run
+create_snapshot "etc-000"
+write_uboot_to_disk
+clean_work_env
+mv ${TGT_IMG} ${OUTPUT_DIR} && sync
+echo "镜像已生成! 存放在 ${OUTPUT_DIR} 下面!"
+echo "========================== end $0 ================================"
+echo
---
+++ myfiles/first_run.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+
+MYSELF=$0
+
+function destory_myself() {
+    rm -f $MYSELF /etc/part_size /tmp/fdisk.script
+    mv -f /etc/rc.local.orig /etc/rc.local
+}
+
+if [ ! -f /etc/part_size ];then
+    echo "/etc/part_size 不存在！"
+    destory_myself
+    exit 1
+fi
+
+# 找到 root 所在的分区
+ROOT_PTNAME=$(df / | tail -n1 | awk '{print $1}' | awk -F '/' '{print $3}')
+if [ "$ROOT_PTNAME" == "" ];then
+    echo "找不到根文件系统对应的分区!"
+    destory_myself
+    exit 1
+fi
+
+# 找到分区所在的磁盘, 仅支持 mmcblk?p?  sd?? hd?? vd??等格式
+case $ROOT_PTNAME in 
+       mmcblk?p[1-4]) DISK_NAME=$(echo $ROOT_PTNAME | awk '{print substr($1, 1, length($1)-2)}');;
+    [hsv]d[a-z][1-4]) DISK_NAME=$(echo $ROOT_PTNAME | awk '{print substr($1, 1, length($1)-1)}');;
+		   *) echo "无法识别 $ROOT_PTNAME 的磁盘类型!"
+		      destory_myself
+		      exit 1
+		   ;;
+esac
+
+CURRENT_PT_CNT=$(fdisk -l /dev/${DISK_NAME} | grep -A4 'Device' | grep -v 'Device' | wc -l)
+if [ "$CURRENT_PT_CNT" != "2" ];then
+    echo "现存分区数量不为2,放弃!"
+    destory_myself
+    exit 1
+fi
+TOTAL_SIZE=$(lsblk -l -b -o NAME,SIZE | awk "\$1 ~ /^${DISK_NAME}\$/ {print \$2}")
+
+TARGET_ROOTFS2_FSTYPE=btrfs
+TARGET_SHARED_FSTYPE=btrfs
+
+SKIP_MB=$(awk '{print $1}' /etc/part_size)
+BOOT_MB=$(awk '{print $2}' /etc/part_size)
+ROOTFS_MB=$(awk '{print $3}' /etc/part_size)
+
+START_P3=$(( (SKIP_MB + BOOT_MB + ROOTFS_MB) * 2048 ))
+END_P3=$((ROOTFS_MB * 2048 + START_P3 -1))
+START_P4=$((END_P3 + 1))
+END_P4=$((TOTAL_SIZE / 512 - 1))
+
+cat > /tmp/fdisk.script <<EOF
+n
+p
+3
+$START_P3
+$END_P3
+n
+p
+$START_P4
+$END_P4
+t
+3
+83
+t
+4
+83
+w
+EOF
+
+echo "Backup the partition table ... "
+dd if=/dev/$DISK_NAME of=/tmp/partition.bak bs=512 count=1
+sync
+echo "done"
+
+echo "fdisk starting ... "
+fdisk /dev/$DISK_NAME < /tmp/fdisk.script
+if [ $? -ne 0 ];then
+	echo "fdisk failed, restore the backup bootloader, and abort"
+	dd if=/tmp/partition.bak of=/dev/$DISK_NAME bs=512 count=1
+	sync
+	destory_myself
+	exit 1
+fi
+echo "fdisk done"
+echo
+
+# mkfs
+case $DISK_NAME in 
+   mmcblk*) PT_PRE=${DISK_NAME}p
+	    LB_PRE="EMMC_"
+	    ;;
+	 *) PT_PRE=${DISK_NAME}
+	    LB_PRE=""
+	    ;;
+esac
+echo "create rootfs2 filesystem ... "
+mkdir -p /mnt/${PT_PRE}3
+case $TARGET_ROOTFS2_FSTYPE in
+	xfs) mkfs.xfs   -f -L "${LB_PRE}ROOTFS2" "/dev/${PT_PRE}3"
+	     mount -t xfs     "/dev/${PT_PRE}3" "/mnt/${PT_PRE}3"
+	     ;;
+      btrfs) mkfs.btrfs -f -L "${LB_PRE}ROOTFS2" "/dev/${PT_PRE}3" 
+	     mount -t btrfs   "/dev/${PT_PRE}3" "/mnt/${PT_PRE}3"
+	     ;; 
+	  *) mkfs.ext4  -F -L "${LB_PRE}ROOTFS2" "/dev/${PT_PRE}3"
+	     mount -t ext4    "/dev/${PT_PRE}3" "/mnt/${PT_PRE}3"
+	     ;;
+esac
+echo "done"
+
+echo "create shared filesystem ... "
+mkdir -p /mnt/${PT_PRE}4
+case $TARGET_SHARED_FSTYPE in
+	xfs) mkfs.xfs   -f -L "${LB_PRE}SHARED" "/dev/${PT_PRE}4"
+	     mount -t xfs     "/dev/${PT_PRE}4" "/mnt/${PT_PRE}4"
+	     ;;
+    btrfs) mkfs.btrfs -f -L "${LB_PRE}SHARED" "/dev/${PT_PRE}4"
+	     mount -t btrfs   "/dev/${PT_PRE}4" "/mnt/${PT_PRE}4"
+	     ;; 
+	*) mkfs.ext4  -F -L "${LB_PRE}SHARED" "/dev/${PT_PRE}4"
+	     mount -t ext4    "/dev/${PT_PRE}4" "/mnt/${PT_PRE}4"
+	     ;;
+esac
+echo "done"
+
+# 新分区建立成功后, 允许在非EMMC设备上也启用docker
+# init dockerd
+# echo "Init the docker configs ... "
+
+# if [ -f /etc/init.d/dockerman ];then
+#     echo -n "stop dockerman ... "
+#     /etc/init.d/dockerman stop
+#     echo "ok"
+
+#     echo -n "disable dockerman ... "
+#     /etc/init.d/dockerman disable
+#     echo "ok"
+# fi
+
+# echo -n "stop docker ... "
+# /etc/init.d/docker stop
+# echo "ok"
+
+# echo -n "disable docker ... "
+# /etc/init.d/docker disable
+# echo "ok"
+
+# mkdir -p "/mnt/${PT_PRE}4/docker"
+# rm -rf "/opt/docker"
+# ln -sf "/mnt/${PT_PRE}4/docker/" "/opt/docker"
+# cat > /etc/docker/daemon.json <<EOF
+# {
+#   "bip": "172.31.0.1/24",
+#   "data-root": "/mnt/${PT_PRE}4/docker/",
+#   "log-level": "warn",
+#   "log-driver": "json-file",
+#   "log-opts": {
+#      "max-size": "10m",
+#      "max-file": "5"
+#    },
+#   "registry-mirrors": [
+#      "https://mirror.baidubce.com/",
+#      "https://hub-mirror.c.163.com"
+#    ]
+# }
+# EOF
+# echo "done"
+
+# echo -n "enable docker ... "
+# /etc/init.d/docker enable
+# echo "ok"
+
+# echo -n "starting docker ... "
+# /etc/init.d/docker start
+# echo "ok"
+
+# if [ -f /etc/init.d/dockerman ];then
+#      if [ -f "/etc/docker/daemon.json" ] && [ -x "/usr/bin/jq" ];then
+#         data_root=$(jq -r '."data-root"' /etc/docker/daemon.json)
+
+#         bip=$(jq -r '."bip"' /etc/docker/daemon.json)
+#         [ "$bip" == "null" ] && bip="172.31.0.1/24"
+
+#         log_level=$(jq -r '."log-level"' /etc/docker/daemon.json)
+#         [ "$log_level" == "null" ] && log_level="warn"
+
+#         _iptables=$(jq -r '."iptables"' /etc/docker/daemon.json)
+#         [ "$_iptables" == "null" ] && _iptables="true"
+
+#         registry_mirrors=$(jq -r '."registry-mirrors"[]' /etc/docker/daemon.json 2>/dev/null)
+#     fi
+
+#     if [ "$data_root" == "" ];then
+#          data_root="/opt/docker/" # the default data root
+#     fi
+
+#     if ! uci get dockerd.globals >/dev/null 2>&1;then
+#         uci set dockerd.globals='globals'
+#         uci commit
+#     fi
+
+#     # delete alter config , use inner config
+#     if uci get dockerd.globals.alt_config_file >/dev/null 2>&1;then
+#         uci delete dockerd.globals.alt_config_file
+#         uci commit
+#     fi
+
+#     uci set dockerd.globals.data_root=$data_root
+#     [ "$bip" != "" ] && uci set dockerd.globals.bip=$bip
+#     [ "$log_level" != "" ] && uci set dockerd.globals.log_level=$log_level
+#     [ "$_iptables" != "" ] && uci set dockerd.globals.iptables=$_iptables
+#     if [ "$registry_mirrors" != "" ];then
+#         for reg in $registry_mirrors;do
+#             uci add_list dockerd.globals.registry_mirrors=$reg
+#         done
+#     fi
+#     uci set dockerd.globals.auto_start='1'
+#     uci commit
+
+#     echo -n "enable dockerman ... "
+#     /etc/init.d/dockerman enable
+#     echo "ok"
+
+#     echo -n "starting dockerman ... "
+#     /etc/init.d/dockerman start
+#     echo "ok"
+# fi
+
+# init AdguardHome
+echo "Init the Adguard config ... "
+if [ -f "/etc/config/AdGuardHome" ];then
+    mkdir -p "/mnt/${PT_PRE}4/AdGuardHome/data"
+    rm -rf "/usr/bin/AdGuardHome"
+    ln -sf "/mnt/${PT_PRE}4/AdGuardHome" "/usr/bin/AdGuardHome"
+fi
+sync
+echo "done"
+echo "clean ... "
+destory_myself
+echo "done"
+echo "The end."
