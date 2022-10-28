e712c65ae59d34bff9b528ca165480f6033d08f9
uniqfreq <flippy@sina.com>
2022-10-28 21:45:00 +0800

    fix r68s bootfiles

---
+++ files/rk3568/r68s/mkbootscr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../../rk3588/rock5b/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/r68s/boot.cmd ../../bootfiles/rockchip/rk3568/r68s/boot.scr
