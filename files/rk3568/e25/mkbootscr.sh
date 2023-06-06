241f7a28b53d70b1620de4529b98578c7d8b6843
uniqfreq <flippy@sina.com>
2023-06-06 13:40:45 +0800

    E25: update bootloader & swap eth0 eth1

---
+++ files/rk3568/e25/mkbootscr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#../../rk3588/rock5b/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/e25/boot.cmd ../../bootfiles/rockchip/rk3568/e25/boot.scr
+/usr/bin/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/e25/boot.cmd ../../bootfiles/rockchip/rk3568/e25/boot.scr
