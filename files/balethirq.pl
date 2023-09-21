9317fcc6e240ca390c470a7ea5dcf46f4e1825f0
uniqfreq <flippy@sina.com>
2023-09-21 21:29:16 +0800

    Rename Hinlink boards to hlink

--- files/balethirq.pl
+++ files/balethirq.pl
@@ -220,22 +220,18 @@ sub board_special_config() {
         &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "FastRhino R68S") {
         &optimize_eth_parameters("eth0","eth1","eth2","eth3");
-    } elsif($board eq "HINLINK OPC-H66K Board") {
-        &optimize_eth_parameters("eth0","eth1","eth2");
-    } elsif($board eq "HINLINK OPC-H68K Board") {
-        &optimize_eth_parameters("eth0","eth1","eth2","eth3");
     } elsif($board eq "Radxa E25") {
         &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "Radxa ROCK 5B") {
         &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "Hlink H28K") {
-        &optimize_eth_parameters("eth1");
+        &optimize_eth_parameters("eth0", "eth1");
     } elsif($board eq "Hlink H66K") {
         &optimize_eth_parameters("eth0","eth1","eth2");
     } elsif($board eq "Hlink H68K") {
         &optimize_eth_parameters("eth0","eth1","eth2","eth3");
     } elsif($board eq "Hlink H69K") {
-        &optimize_eth_parameters("eth0","eth1","eth2");
+        &optimize_eth_parameters("eth0","eth1","eth2","eth3");
     }
 }
 
