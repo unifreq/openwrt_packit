615199af09ffda6fc64fbf09c86597d42707271a
unifreq <flippy@sina.com>
2025-11-25 16:17:39 +0800

    Add rk3576 100ask dshanpi-a1 base files

---
+++ files/rk3576/100ask-dshanpi-a1/mkbootscr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3576/100ask-dshanpi-a1/boot.cmd ../../bootfiles/rockchip/rk3576/100ask-dshanpi-a1/boot.scr
