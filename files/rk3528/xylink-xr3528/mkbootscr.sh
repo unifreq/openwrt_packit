41f85e321bdc16f24f4c0b703b72ec128df0acf5
uniqfreq <flippy@sina.com>
2026-07-24 11:29:04 +0800

    board: add xylink xr3528

---
+++ files/rk3528/xylink-xr3528/mkbootscr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#/usr/bin/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3528/h28k/boot.cmd ../../bootfiles/rockchip/rk3528/h28k/boot.scr
+../../rk3588/rock5b/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3528/h28k/boot.cmd ../../bootfiles/rockchip/rk3528/h28k/boot.scr
